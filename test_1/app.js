094397a59fda2435562becf6357572898f26df1e
d1_codes <anekemikeobiora2@gmail.com>
2022-08-18 18:53:24 +0100

    File update

---
+++ test_1/app.js
@@ -0,0 +1,16 @@
+const mobileNav = document.querySelector('.mobileHeader')
+const cancelIcon = document.querySelector('#cancelIcon')
+const hamBurgerMenu = document.querySelector('.hamBurger')
+const containers = document.querySelectorAll('.container')
+const buttons = document.querySelectorAll('.container div div button')
+const headers = document.querySelectorAll('.container div h3')
+
+
+function handleHamburgerClick(){
+    mobileNav.style.display = 'block';
+}
+
+hamBurgerMenu.addEventListener('click', handleHamburgerClick)
+cancelIcon.addEventListener('click', ()=>{
+    mobileNav.style.display = 'none'
+})
